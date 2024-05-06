81158a995d95ea40af971d0ece39ec93a2f76291
David Williamson <williamsondavidv@outlook.com>
2024-05-06 13:59:33 -0500

    troubleshooting db

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
-
+app.app_context().push()
 connect_db(app)
 db.create_all()
 
